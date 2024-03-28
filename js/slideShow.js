42ef91af9d6bbe34c78dcad52a84bf8901c35721
denissakay <164032359+denissakay@users.noreply.github.com>
2024-03-28 21:33:43 +0700

    Add files via upload
    PROJECT SUBMISSION

---
+++ js/slideShow.js
@@ -0,0 +1,24 @@
+const heroSlider = document.getElementById('hero-slider');
+const windowWidth = heroSlider.clientWidth;
+
+let index = 1;
+
+heroSlider.style.transform = `translateX(${-windowWidth * index}px)`;
+
+setInterval(() => {
+	if (index == 3) return;
+
+	index++;
+
+	heroSlider.style.transform = `translateX(${-windowWidth * index}px)`;
+	heroSlider.style.transition = 'all 2s ease-in-out';
+}, 3000);
+
+heroSlider.addEventListener('transitionend', () => {
+	if (document.getElementById(`slide${index}`).id == 'slide3') {
+		heroSlider.style.transition = 'none';
+		index = 0;
+		heroSlider.style.transform = `translateX(${-windowWidth * index}px)`;
+	}
+})
+
